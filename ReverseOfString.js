6172b086214960603d8cbdbd6867c793cc63a4fc
BalajiB <Balaji.B@ellucian.com>
2023-04-23 17:04:02 +0530

    2nd program

---
+++ ReverseOfString.js
@@ -0,0 +1,15 @@
+//Write a program that takes a string and returns the reverse of that string.
+
+let str = "hello";
+let chArrStr = str.split('');
+let revStr='';
+
+for(let i=chArrStr.length-1;i>=0;i--){
+    revStr+=chArrStr[i];
+}
+
+console.log('Original String = '+str);
+console.log('Reversed String = '+revStr);
+
+let revStrmethod=chArrStr.reverse();
+console.log('Method Reversed String = '+revStrmethod);
