27921e35092dced7996885b8e339d84b79a71db2
BalajiB <Balaji.B@ellucian.com>
2023-04-29 22:41:01 +0530

    homework

--- HW.js
+++ HW.js
@@ -5,21 +5,8 @@ let arr = [
   { id: 20, name: 'Jane', age: 25 }
 ];
 
-let first= "Hi, my name is ";
-let second= " I'm ";
-let third= " year's old";
-// age1 = arr[0].age
-// age2 = arr[1].age
-// age3 = arr[2].age
+arr.sort((a,b)=>b.age-a.age);
 
-let arrAge = [age1,age2,age3];
-
-// console.log(typeof arr[0]);
-
-// arrAge.sort((arr[0],arr[1]) => arr[1].id-arr[0].id);
-
-for(let obj in arr){
-	if(arr[obj].age=arrAge[obj]){
-		console.log(first + arr[obj].name + second + arr[obj].age + third)
-	}	
+for(let obj in arr){	
+		console.log("Hi, my name is " + arr[obj].name + " I'm " + arr[obj].age + " year's old " + ",my ID : "+arr[obj].id);
 }
